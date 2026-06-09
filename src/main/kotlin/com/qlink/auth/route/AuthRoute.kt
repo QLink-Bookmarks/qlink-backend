0c863ffdca1e54f57c17b4a03b62a05f8b2f5b9c
Injae Song <injaesong0@gmail.com>
2026-06-09 12:06:43 +0900

    Fix auth web refresh CSRF route

--- src/main/kotlin/com/qlink/auth/route/AuthRoute.kt
+++ src/main/kotlin/com/qlink/auth/route/AuthRoute.kt
@@ -10,12 +10,21 @@ import com.qlink.common.error.BusinessException
 import com.qlink.common.error.ErrorCode
 import com.qlink.common.response.respondError
 import com.qlink.common.response.respondSuccess
+import io.github.smiley4.ktoropenapi.documentation
+import io.github.smiley4.ktoropenapi.config.RouteConfig
+import io.github.smiley4.ktoropenapi.resources.extractTypesafeDocumentation
 import io.github.smiley4.ktoropenapi.resources.post
+import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.resources.Resources
+import io.ktor.server.resources.handle
+import io.ktor.server.resources.resource
 import io.ktor.server.plugins.csrf.CSRF
 import io.ktor.server.request.receive
+import io.ktor.server.routing.RoutingContext
 import io.ktor.server.routing.Route
-import io.ktor.server.routing.route
+import io.ktor.server.routing.method
+import kotlinx.serialization.serializer
 import org.koin.ktor.ext.inject
 
 const val REFRESH_TOKEN_HEADER_NAME = "refresh_token"
@@ -47,7 +56,7 @@ fun Route.authRoutes() {
 }
 
 private fun Route.webRefreshRoute(refreshAuthTokenService: RefreshAuthTokenService) {
-    route("") {
+    resource<AuthResources.Token.Refresh.Web> {
         install(CSRF) {
             checkHeader(CSRF_TOKEN_HEADER_NAME)
             onFailure { reason ->
@@ -58,7 +67,7 @@ private fun Route.webRefreshRoute(refreshAuthTokenService: RefreshAuthTokenServi
             }
         }
 
-        post<AuthResources.Token.Refresh.Web>(webRefreshAuthTokenDocs()) {
+        openApiPost<AuthResources.Token.Refresh.Web>(webRefreshAuthTokenDocs()) {
             val refreshToken =
                 call.request.headers[REFRESH_TOKEN_HEADER_NAME]
                     ?.takeIf { it.isNotBlank() }
@@ -69,4 +78,24 @@ private fun Route.webRefreshRoute(refreshAuthTokenService: RefreshAuthTokenServi
             call.respondSuccess(HttpStatusCode.Created, response)
         }
     }
+}
+
+private inline fun <reified T : Any> Route.openApiPost(
+    noinline builder: RouteConfig.() -> Unit = { },
+    noinline body: suspend RoutingContext.(T) -> Unit,
+): Route {
+    val resources = plugin(Resources)
+    val extractedDocumentation = extractTypesafeDocumentation(serializer<T>(), resources.resourcesFormat)
+    lateinit var builtRoute: Route
+
+    documentation(extractedDocumentation) {
+        documentation(builder) {
+            builtRoute =
+                method(HttpMethod.Post) {
+                    handle<T>(body)
+                }
+        }
+    }
+
+    return builtRoute
 }
