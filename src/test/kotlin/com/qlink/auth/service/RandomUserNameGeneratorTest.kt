b0f5e459bdc2208a3c7e809dbfe0afb446bf8953
Injae Song <injaesong0@gmail.com>
2026-06-14 14:07:11 +0900

    Fix random auth username generation

--- src/test/kotlin/com/qlink/auth/service/RandomUserNameGeneratorTest.kt
+++ src/test/kotlin/com/qlink/auth/service/RandomUserNameGeneratorTest.kt
@@ -1,8 +1,11 @@
 package com.qlink.auth.service
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldMatch
 import io.kotest.matchers.string.shouldNotBeBlank
 import io.kotest.matchers.string.shouldNotContain
+import io.kotest.matchers.string.shouldNotMatch
 
 class RandomUserNameGeneratorTest :
     BehaviorSpec({
@@ -16,6 +19,16 @@ class RandomUserNameGeneratorTest :
                     generatedName.username.shouldNotBeBlank()
                     generatedName.username shouldNotContain "-"
                     generatedName.nickname.shouldNotBeBlank()
+                }
+
+                Then("nickname은 랜덤 한글 이름으로 생성한다") {
+                    generatedName.nickname shouldContain " "
+                    generatedName.nickname shouldNotMatch Regex(".*[0-9a-f]{6}$")
+                }
+
+                Then("username은 랜덤 영문 이름과 uuid 파편으로 생성한다") {
+                    generatedName.username shouldMatch Regex("[a-z]+[0-9a-f]{6}")
+                    generatedName.username shouldNotMatch Regex("[0-9a-f]{32}")
                 }
             }
         }
